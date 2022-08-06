b4606b06ba2632413dc53b5bdcbf871850b64b41
Gavryk <gvozdyk23@gmail.com>
2022-08-06 12:18:06 +0300

    widget: filter set active category

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import { UIDropdown } from './components';
 import { MainLayout } from './layouts';
 import { FilterWidget } from './widgets';
 
-const catList = ['Meat', 'Vegetarian', 'Grill', 'Sharp', 'Closed'];
-const sortList = [
-  { name: 'Popular', type: 'rating', order: 'desc' },
-  { name: 'Price', type: 'price', order: 'desc' },
-  { name: 'Alphabet', type: 'name', order: 'asc' },
-];
-
 const App = () => {
   return (
     <MainLayout>
-      <FilterWidget catList={catList} sortList={sortList} />
+      <FilterWidget />
     </MainLayout>
   );
 };
--- src/widgets/filter-widget/index.jsx
+++ src/widgets/filter-widget/index.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UIButton, UIDropdown } from '../../components';
 import styles from './styles.module.scss';
 
-const FilterWidget = ({ catList, sortList }) => {
+const catList = ['Meat', 'Vegetarian', 'Grill', 'Sharp', 'Closed'];
+
+const sortList = [
+  { name: 'Popular', type: 'rating', order: 'desc' },
+  { name: 'Price', type: 'price', order: 'desc' },
+  { name: 'Alphabet', type: 'name', order: 'asc' },
+];
+
+const FilterWidget = () => {
+  const [activeCat, setActiveCat] = useState(null);
+
   return (
     <div className={styles.filterWrapper}>
       <div className={styles.categoriesWrap}>
-        <UIButton active>All</UIButton>
-        {catList && catList.map((cat, index) => <UIButton key={`${cat}_${index}`}>{cat}</UIButton>)}
+        <UIButton active={activeCat === null && true} onClick={() => setActiveCat(null)}>
+          All
+        </UIButton>
+        {catList &&
+          catList.map((cat, index) => (
+            <UIButton
+              key={`${cat}_${index}`}
+              active={activeCat === index && true}
+              onClick={() => setActiveCat(index)}>
+              {cat}
+            </UIButton>
+          ))}
       </div>
-      <div className={styles.sortWrap}>
-        <UIDropdown sortList={sortList} />
-      </div>
+      <UIDropdown sortList={sortList} />
     </div>
   );
 };
