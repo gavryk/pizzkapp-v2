9ae9d08b0ac1e156eedede54b909935eaa9d1895
Gavryk <gvozdyk23@gmail.com>
2022-08-07 18:33:15 +0300

    backend: replace all pizzas from fake db.json on mockapi server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MainLayout } from './layouts';
 import { Home } from './pages';
 
-import pizzas from './assets/db.json';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 
 const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://62efd80f8d7bc7c2eb81138f.mockapi.io/pizzas').then(({ data }) => {
+      setPizzas(data);
+    });
+  }, []);
+
   return (
     <MainLayout>
       <Routes>
