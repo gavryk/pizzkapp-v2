b7abc09bc1585fa8294ff26f12fe8889fc0efef5
Gavryk <gvozdyk23@gmail.com>
2022-08-08 01:22:13 +0300

    ui: added skeleton card rfc

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { UICard, UIGrid, UITitle, SkeletonCard } from '../../components';
 import { FilterWidget } from '../../widgets';
 
