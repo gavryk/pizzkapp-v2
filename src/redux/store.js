e2243ac6accd4bc8fa473c414acaa638e86d2a55
Gavryk <gvozdyk23@gmail.com>
2022-09-04 23:46:44 +0300

    redux: install RTK

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,6 @@
-import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducers from './reducers';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
-
-window.store = store;
-
-export default store;
+export const store = configureStore({
+  reducer: rootReducers,
+});
