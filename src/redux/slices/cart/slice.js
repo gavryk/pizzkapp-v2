7e92d59dd8f60e4979a90e25e51a64ccc0255ffc
Gavryk <gvozdyk23@gmail.com>
2022-09-12 00:06:02 +0300

    redux: cart slice some functionality (not finish)

--- src/redux/slices/cart/slice.js
+++ src/redux/slices/cart/slice.js
@@ -12,7 +12,10 @@ const cartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
       state.items.push(action.payload);
-      state.totalCount = [...state.items, action.payload].length;
+      state.totalCount = [...state.items, action.payload].length - 1;
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price + sum;
+      }, 0);
     },
     removeItem: (state, action) => {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
