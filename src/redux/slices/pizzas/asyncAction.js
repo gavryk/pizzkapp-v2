05f61047630bb71154f211ec82754e6894ac3385
Gavryk <gvozdyk23@gmail.com>
2022-10-20 01:13:41 +0300

    ui: error message, some little refactor

--- src/redux/slices/pizzas/asyncAction.js
+++ src/redux/slices/pizzas/asyncAction.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const dbmock = 'https://62f6ca0ba3bce3eed7c7ca7a.mockapi.io';
 
-export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params) => {
+export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params, thunkAPI) => {
   const { category, sortBy, searchText, currentPage } = params;
   let catQuery = category !== null ? `category=${category}&` : '';
   let searchQuery = searchText !== null ? `&search=${searchText}` : '';
