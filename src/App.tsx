304790cc4cbd9dccce4f721f17710e22bfcbff28
Gavryk <gvozdyk23@gmail.com>
2022-11-21 07:14:10 +0200

    installed react-loadable, just test. Looks better React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,38 @@
-import React, { lazy, Suspense } from 'react';
+import Loadable from 'react-loadable';
+import React from 'react';
 import { MainLayout } from './layouts';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import { Spinner } from './components';
 
-const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ './pages/cart'));
-const NotFound = lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/404'));
-const SinglePizza = lazy(
-  () => import(/* webpackChunkName: "SinglePizza" */ './pages/pizzas/single'),
-);
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: "Cart" */ './pages/cart'),
+  loading: () => <Spinner />,
+});
+const NotFound = Loadable({
+  loader: () => import(/* webpackChunkName: "NotFound" */ './pages/404'),
+  loading: () => <Spinner />,
+});
+const SinglePizza = Loadable({
+  loader: () => import(/* webpackChunkName: "SinglePizza" */ './pages/pizzas/single'),
+  loading: () => <Spinner />,
+});
+
+//React Lazy
+// const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ './pages/cart'));
+// const NotFound = lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/404'));
+// const SinglePizza = lazy(
+//   () => import(/* webpackChunkName: "SinglePizza" */ './pages/pizzas/single'),
+// );
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route
-          path="cart"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <Cart />
-            </Suspense>
-          }
-        />
-        <Route
-          path="pizza/:id"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <SinglePizza />
-            </Suspense>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <NotFound />
-            </Suspense>
-          }
-        />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<SinglePizza />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
